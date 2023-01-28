295cba7820d6f7eb63e558116afcf0485e778b20
muhammadumairmoosani <muhammadumairmoosani@gmail.com>
2023-01-28 23:10:45 +0500

    MOD charactor counter to word counter

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const paragraph = await inquirer.prompt({
 });
 const { paragraphText } = paragraph;
 if (paragraphText) {
-    console.log(chalk.green(`Type paragraph total work is ${paragraphText.replace(/ /g, "").length}`));
+    console.log(chalk.green(`Type paragraph total work is ${paragraphText.split(" ").length}`));
 }
 else {
     console.log(chalk.red("Something when wrong!"));
--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const paragraph: {
 const { paragraphText } = paragraph;
 
 if(paragraphText) {
-    console.log(chalk.green(`Type paragraph total work is ${paragraphText.replace(/ /g,"").length}`))
+    console.log(chalk.green(`Type paragraph total work is ${paragraphText.split(" ").length}`))
 } else {
     console.log(chalk.red("Something when wrong!"))
 }
